9ff66ace2ad5cce2342d87b3f92d553b2806921e
minjong kim <alswhddkdl@gmail.com>
2025-01-24 23:28:32 +0900

    feat: add button

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -41,6 +41,13 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
+
+          ElevatedButton(
+            onPressed: () {
+            print("button pressed");
+            }, 
+            child: Text("Next")
+            )
         ],
       ),
     );
