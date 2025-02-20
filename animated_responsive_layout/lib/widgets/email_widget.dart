b0023ef9ef0e193ebfccf977c3d90070e543a762
minjong kim <alswhddkdl@gmail.com>
2025-02-20 23:22:44 +0900

    [animated responsive] feat: add email_content_state

--- animated_responsive_layout/lib/widgets/email_widget.dart
+++ animated_responsive_layout/lib/widgets/email_widget.dart
@@ -93,3 +93,135 @@ class EmailContent extends StatefulWidget {
   @override
   State<EmailContent> createState() => _EmailContentState();
 }
+
+class _EmailContentState extends State<EmailContent> {
+  late final ColorScheme _colorScheme = Theme.of(context).colorScheme;
+  late final TextTheme _textTheme = Theme.of(context).textTheme;
+
+  // 공백 컴포넌트 화
+  Widget get contentSpacer => SizedBox(height: widget.isThreaded ? 20 : 2);
+
+  // 현재와 마지막 활성화 시간을 계산하는 변수수
+  String get lastActiveLabel {
+    final DateTime now = DateTime.now();
+    if (widget.email.sender.lastActive.isAfter(now)) throw ArgumentError();
+    final Duration elapsedTime =
+        widget.email.sender.lastActive.difference(now).abs();
+    return switch (elapsedTime) {
+      Duration(inSeconds: < 60) => '${elapsedTime.inSeconds}s',
+      Duration(inMinutes: < 60) => '${elapsedTime.inMinutes}m',
+      Duration(inHours: < 24) => '${elapsedTime.inHours}h',
+      Duration(inDays: < 365) => '${elapsedTime.inDays}d',
+      _ => throw UnimplementedError(),
+    };
+  }
+
+  // 위젯에 따른 글 스타일 변경경
+  TextStyle? get contentTextStyle => switch (widget) {
+        EmailContent(isThreaded: true) => _textTheme.bodyLarge,
+        EmailContent(isSelected: true) => _textTheme.bodyMedium
+            ?.copyWith(color: _colorScheme.onPrimaryContainer),
+        _ =>
+          _textTheme.bodyMedium?.copyWith(color: _colorScheme.onSurfaceVariant),
+      };
+
+
+  // 메인 UI
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          LayoutBuilder(builder: (context, constraints) {
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (constraints.maxWidth - 200 > 0) ...[
+                  CircleAvatar(
+                    backgroundImage: AssetImage(widget.email.sender.avatarUrl),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(horizontal: 6.0)),
+                ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.email.sender.name.fullName,
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                        style: widget.isSelected
+                            ? _textTheme.labelMedium?.copyWith(
+                                color: _colorScheme.onSecondaryContainer)
+                            : _textTheme.labelMedium
+                                ?.copyWith(color: _colorScheme.onSurface),
+                      ),
+                      Text(
+                        lastActiveLabel,
+                        overflow: TextOverflow.fade,
+                        maxLines: 1,
+                        style: widget.isSelected
+                            ? _textTheme.labelMedium?.copyWith(
+                                color: _colorScheme.onSecondaryContainer)
+                            : _textTheme.labelMedium?.copyWith(
+                                color: _colorScheme.onSurfaceVariant),
+                      ),
+                    ],
+                  ),
+                ),
+                if (constraints.maxWidth - 200 > 0) ...[
+                  const StarButton(),
+                ]
+              ],
+            );
+          }),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (widget.isPreview) ...[
+                Text(
+                  widget.email.subject,
+                  style: const TextStyle(fontSize: 18)
+                      .copyWith(color: _colorScheme.onSurface),
+                ),
+              ],
+              if (widget.isThreaded) ...[
+                contentSpacer,
+                Text(
+                  "To ${widget.email.recipients.map((recipient) => recipient.name.first).join(", ")}",
+                  style: _textTheme.bodyMedium,
+                )
+              ],
+              contentSpacer,
+              Text(
+                widget.email.content,
+                maxLines: widget.isPreview ? 2 : 100,
+                overflow: TextOverflow.ellipsis,
+                style: contentTextStyle,
+              ),
+            ],
+          ),
+          const SizedBox(width: 12),
+          widget.email.attachments.isNotEmpty
+              ? Container(
+                  height: 96,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(widget.email.attachments.first.url),
+                    ),
+                  ),
+                )
+              : const SizedBox.shrink(),
+          if (!widget.isPreview) ...[
+            const EmailReplyOptions(),
+          ],
+        ],
+      ),
+    );
+  }
+}
